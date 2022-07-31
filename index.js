bab498bb29a4d2b1e651ac24ec40edcf82913737
Aditya Kamble <adityakamble6101@gmail.com>
2022-07-31 22:31:14 +0530

    cors error fix.

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const dotenv = require("dotenv").config();
 const app = express();
 const connectDB = require("./config/db");
 const weather = require("./routes/weatherRoutes");
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 // connect to db
 connectDB();
